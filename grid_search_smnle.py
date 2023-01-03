07d8c6540688b29e06ccdd39a61d85a6945315a5
Pierre Glaser <pierreglaser@msn.com>
2023-01-03 14:51:28 +0000

    minor changes to grid_search_smnle.py
    also add a sample results file from the grid search

--- grid_search_smnle.py
+++ grid_search_smnle.py
@@ -2,20 +2,26 @@ import itertools
 import shutil
 from pathlib import Path
 
+from dask.distributed import CancelledError
+
 from smnle import smnle
+
+TECHNIQUES = ("SSM", "SM")[:1]
 
 TASKS = (
     "two_moons",
     "gaussian_linear_uniform",
     "slcp",
     "lotka_volterra",
-)
+)[1:2]
 
 # talk about neural net architecture
 
-OBSERVATIONS = list(range(1, 11))
+OBSERVATIONS = list(range(1, 11))[:1]
 
-LRS = [0.01, 0.005, 0.001]
+LRS = [0.01, 0.001][1:]
+
+NUM_SAMPLES = [1000, 10000, 100000][1:]
 
 if __name__ == "__main__":
 
@@ -29,11 +35,13 @@ if __name__ == "__main__":
 
         cluster = SLURMCluster(  # type: ignore
             n_workers=0,  # create the workers "lazily" (upon cluster.scale)
-            memory="4GB",  # amount of RAM per worker
+            memory="24GB",  # amount of RAM per worker
             processes=1,  # number of execution units per worker (threads and processes)
             cores=1,  # among those execution units, number of processes
             job_extra=[
                 # '--export=ALL', # default behavior.
+                "-p gpu",  # partition
+                "--gres=gpu:1",  # number of GPUs per node
                 "--output=dask-simulation-logs/R-%x.%j.out",
                 "--error=dask-simulation-logs/R-%x.%j.err",
                 *(),
@@ -49,7 +57,8 @@ if __name__ == "__main__":
             walltime="24:0:0",
         )
         print(cluster.job_script())
-        cluster.scale(50)
+        cluster.adapt(minimum_jobs=1, maximum_jobs=40)
+        # cluster.scale(20)
     else:
         from dask.distributed import LocalCluster
         cluster = LocalCluster(n_workers=4)
@@ -61,43 +70,69 @@ if __name__ == "__main__":
     print(client.submit(lambda x: x + 1, 10).result())
     print('cluster is working')
 
+    import jax.numpy as jnp
+    futs = [client.submit(lambda x: str(jnp.ones((2,)).device()), i) for i in range(50)]
+    print([f.result() for f in futs])
+
     futures = []
     args = []
 
     grid = list(
         itertools.product(
+            TECHNIQUES,
             TASKS,
             OBSERVATIONS,
-            LRS
+            LRS,
+            NUM_SAMPLES,
         )
     )
 
     for arg in grid:
-        task, num_observation, lr = arg
+        technique, task, num_observation, lr, num_samples = arg
         fut = client.submit(
-            smnle,
+            lambda orig_args, **kwargs: (orig_args, smnle(**kwargs)),
+            arg,
             model=task,
+            technique=technique,
             num_observation=num_observation,
             SM_lr=lr,
             SM_lr_theta=lr,
+            # batch_size=min(num_samples, 10000),
             epochs=500,
-            mcmc_num_chains=50,
-            mcmc_num_warmup_steps=1000,
+            n_samples_training=num_samples,
+            # epochs=500,
+            # mcmc_num_warmup_steps=2000,
+            mcmc_num_chains=100,
+            mcmc_num_warmup_steps=5000,
             num_posterior_samples=10000,
+            # batch_size=10000,
+            cuda=True
         )
         futures.append(fut)
         args.append(arg)
 
-    # wait till all dask futures are done
-    client.gather(futures)
+    results ={}
 
-    results = [fut.result() for fut in futures]
-    print(dict(zip(args, results)))
+    from dask.distributed import as_completed
+    num_finished = 0
+    num_remaining = len(futures)
+    for fut in as_completed(futures):
+        ret = fut.result()  # type: ignore
+        arg, result  = ret
 
-    with open("results.pkl", "wb") as f:
-        import pickle
-        pickle.dump(zip(args, results), f)
+        num_finished += 1
+        num_remaining -= 1
+        print('finished', arg)
+        print('num_finished', num_finished, 'num_remaining', num_remaining)
 
+
+        results[arg] = result
+
+        with open("results_rec.pkl", "wb") as f:
+            import pickle
+            pickle.dump(results, f)
+
+    print('done, shutting down')
     client.shutdown()
     cluster.close()
 
